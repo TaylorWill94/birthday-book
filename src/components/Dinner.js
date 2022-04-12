f4f55de39dbf8b38149bb1617a649dfd9effae70
Taylor W <taylorwilliams@pursuit.org>
2022-04-12 15:58:44 -0400

    Added Look 04

--- src/components/Dinner.js
+++ src/components/Dinner.js
@@ -2,6 +2,8 @@ import gucci_suit from "../Images/Gucci-Jumpsuit.jpg";
 import dolce from "../Images/dolce.jpg";
 import area from "../Images/area-dress.jpg";
 import ysl from "../Images/ysl-heels.jpg";
+import laquan from "../Images/laquan.jpg";
+import dolce_gabbana from "../Images/dolce_gabbana.jpg";
 
 function Dinner() {
   return (
@@ -95,6 +97,51 @@ function Dinner() {
           <br />
           <a
             href="https://www.saksfifthavenue.com/product/saint-laurent-opyum-leather-sandals-0400099522701.html?dwvar_0400099522701_color=BLACK"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="purchase">PURCHASE</button>
+          </a>
+          <br />
+          <br />
+        </div>
+        <br />
+        <div className="column">
+          <br />
+          LOOK 03
+          <br />
+          <br />
+          <p>laquan smith jumpsuit</p>
+          <br />
+          <a
+            href="https://laquansmith.com/collections/catsuit/products/off-the-shoulder-sheer-and-velvet-catsuit"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={laquan} alt="laquan-smith" width="200" />
+          </a>
+          <br />
+          <br />
+          <a
+            href="https://laquansmith.com/collections/catsuit/products/off-the-shoulder-sheer-and-velvet-catsuit"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="purchase">PURCHASE</button>
+          </a>
+          <br />
+          <br />
+          <p>dolce & gabbana heels</p>
+          <a
+            href="https://www.saksfifthavenue.com/product/dolce-gabbana-sculpted-heel-patent-leather-sandals-0400012630540.html?dwvar_0400012630540_color=NERO"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={dolce_gabbana} alt="dolce & gabbana heels" width="200" />
+          </a>
+          <br />
+          <a
+            href="https://www.saksfifthavenue.com/product/dolce-gabbana-sculpted-heel-patent-leather-sandals-0400012630540.html?dwvar_0400012630540_color=NERO"
             target="_blank"
             rel="noreferrer"
           >
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,25 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <main className="home">
       Choose your fit!
       <br />
       <br />
-      <button className="btn">BIRTHDAY DINNER LOOK</button>
+      <Link to="/dinner">
+        <button className="btn">BIRTHDAY DINNER LOOK</button>
+      </Link>
       <br />
       <br />
-      <button className="btn">BIRTHDAY LOOK</button>
+      <Link to="/birthday">
+        {" "}
+        <button className="btn">BIRTHDAY LOOK</button>
+      </Link>
       <br />
       <br />
-      <button className="btn">VACATION LOOKS</button>
+      <Link to="/vacation">
+        <button className="btn">VACATION LOOKS</button>
+      </Link>
       <br />
       <br />
     </main>
